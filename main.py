121f467e7587757421bcc5c406cbe2bfed44ec6e
bguzman3-wq <bguzman3@uco.edu>
2026-09-02 10:56:06 -0500

    first commit test

---
+++ main.py
@@ -0,0 +1,7 @@
+print("hello")
+probabilities = [0.60, 0.70, 0.80, 0.90]
+total_probability = 1
+for probability in probabilities:
+    total_probability *= probability
+total_probability = round(total_probability, 4)
+print (total_probability)
